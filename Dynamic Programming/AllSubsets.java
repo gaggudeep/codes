2acb943848583fce339c219923d86909efb93460
Gagandeep Singh <gagandeepsinghjubbal@gmail.com>
2020-09-23 15:49:40 +0530

    Easier All Subsets solution

--- Dynamic Programming/AllSubsets.java
+++ Dynamic Programming/AllSubsets.java
@@ -2,64 +2,23 @@ import java.util.*;
 
 class AllSubsets {
 
-	static ArrayList<ArrayList<Integer>> getAllSubsets(int[] set, int index) {
-
-		ArrayList<ArrayList<Integer>> allSubsets; 
-		if(set.length == index) {
-
-			allSubsets = new ArrayList<>();
-			allSubsets.add(new ArrayList<Integer>());
-		}
-		else {
-
-			allSubsets = getAllSubsets(set, index + 1);
-			int element = set[index];
-			ArrayList<ArrayList<Integer>> moreSubsets = new ArrayList<>();
-			for(ArrayList<Integer> subsets : allSubsets) {
-
-				ArrayList<Integer> newSubset = new ArrayList<>();
-				newSubset.addAll(subsets);
-				newSubset.add(element);
-				//System.out.println(subsets + " and " + element + " added to newSubset.");
-				moreSubsets.add(newSubset);
-				//System.out.println(newSubset + " added to moreSubsets");
+	static void subsets(int[] a)
+	{
+		for(int i = 0 ; i < (1 << a.length) ; i++)
+		{
+			System.out.print("[ ");
+			for(int j = 0 ; j < a.length ; j++)
+			{
+				if((i & (1 << j)) > 0)
+					System.out.print(a[j] + " ");
 			}
-			allSubsets.addAll(moreSubsets);
-			//System.out.println(moreSubsets + " added to allSubsets.");
+			System.out.println(']');
 		}
-		return allSubsets;
 	}
 
-	static ArrayList<ArrayList<Integer>> getSubsets(int[] set) {
-
-		ArrayList<ArrayList<Integer>> allSubsets = new ArrayList<>();
-		int max = 1 << set.length;
-		for(int k = 0; k < max ; k++) {
-
-			ArrayList<Integer> subset = convertIntToSet(k, set);
-			allSubsets.add(subset);
-		}
-		return allSubsets;
-	}
-
-	static ArrayList<Integer> convertIntToSet(int x, int[] set) {
-
-		ArrayList<Integer> subset = new ArrayList<>();
-		int index = 0;
-		for(int k = x ; k > 0 ; k>>= 1) {
-
-			if((k & 1) == 1) {
-
-				subset.add(set[index]);
-			}
-			index++;
-		}
-		return subset;
-	}
-
-	public static void main(String args[]) {
-
-		int[] set = {1, 2, 3};
-		System.out.println(getSubsets(set));
+	public static void main(String args[])
+	{
+		int[] a = {1,2,3,4};
+		subsets(a);
 	}
 }
