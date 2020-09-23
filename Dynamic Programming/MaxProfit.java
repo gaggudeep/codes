e8faf36827421adbc73c5a919cee877d9550ee5e
Gagandeep Singh <gagandeepsinghjubbal@gmail.com>
2020-09-23 14:18:48 +0530

    Meaningful var names

--- Dynamic Programming/MaxProfit.java
+++ Dynamic Programming/MaxProfit.java
@@ -4,12 +4,12 @@ class MaxProfit {
     public int maxProfit(int[] prices) {
         if(prices.length == 0)
             return 0;
-        int maxProfit = 0, min = prices[0];
+        int maxProfitSoFar = 0, minSoFar = prices[0];
         for(int i  = 1 ; i < prices.length ; i++)
         {
-            maxProfit= Math.max(maxProfit, prices[i] - min);
-            min = Math.min(min, prices[i]);
+            maxProfitSoFar= Math.max(maxProfitSoFar, prices[i] - minSoFar);
+            minSoFar = Math.minSoFar(minSoFar, prices[i]);
         }
-        return maxProfit;
+        return maxProfitSoFar;
     }
 }
